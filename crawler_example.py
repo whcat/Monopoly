56137bf4813aaaa43ae7c0439b85ae9986f3aeb1
Ray_Chen <ray7476@gmail.com>
2019-02-02 16:04:01 +0800

    modify file

--- crawler_example.py
+++ crawler_example.py
@@ -8,36 +8,38 @@ import Quantative_analysis as qa
 #read stock file
 
 # stockdf = pd.read_csv('./stocklist/8446_stockinfo.csv')
-
-stock = Stockcrawler.StockCrawler('1215')
+#
+##=======Append and crawler stock info=======================
+#stock = Stockcrawler.StockCrawler('1215')
+##
+#st = stock.AppendStockdf()
+#
+#print(st)
 #
 
-#Append stock info
-#
-#
-st = stock.AppendStockdf()
-
-print(st)
+stocklist = pd.read_csv('./stocklist/StockList.csv')
+print(stocklist)
 
 
+stocklist = [2926,2886,6596]
+
+for i in stocklist:
+    try :
+        stock = Stockcrawler.Stockcrawler(str(i))
+        st = stock.AppendStockdf()
+    except:
+        pass
+
+    time.sleep(3600)
+print("crawler done")
+
+
+##======= Update stocklist to the Last=======================
 # Update to the Last stock info
 # st = ST.UpdateTotheLast()
 
 
 
-
-#
-# stockdf.to_csv('./stocklist/2330_stockinfo.csv')
-
-# alist = list(stockdf["date"])
-#
-# for i,e in enumerate(stockdf[date]):
-#     alist[i] = e.split(" ")[0]
-#
-
-# print(alist)
-# print(list(stockdf['turnover']))
-# print(stockdf)
 
 
 
